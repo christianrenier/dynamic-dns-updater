04da36ba67db515204ecc020976089038ca51ade
Christian Renier <chris@christianrenier.com>
2012-08-02 17:00:53 -0400

    Implemented basic error checking.
    Used ugly try,except statements for basic error handling. Reading
    from and writing to the IP cache, checking current public facing
    IP address, and updating the Dynamic DNS are now all wrapped in
    exception handling.

--- __main__.py
+++ __main__.py
@@ -13,13 +13,21 @@ logger = utils.logger.Logger(log_file_location)
 
 # Retrieve IP address stored from previous run
 ip_cache_location = parser.get('general', 'ip_cache_file')
-cacher = utils.cacher.Cacher(ip_cache_location)
+try:
+	cacher = utils.cacher.Cacher(ip_cache_location)
+except:
+	logger.log_error('Problem reading from IP cache file.')
+	sys.exit()
 old_ip = cacher.get_ip()
 
 # Retrieve current public facing IP address
 site_list_location = os.path.dirname(__file__) + '/sitelist.txt'
 checker = utils.checker.Checker(site_list_location)
-current_ip = checker.get_ip()
+try:
+	current_ip = checker.get_ip()
+except:
+	logger.log_error('Problem checking your current IP address.')
+	sys.exit()
 
 # Exit this script if IP address has not changed
 if old_ip == current_ip:
@@ -31,10 +39,19 @@ if old_ip == current_ip:
 # Update Dynamic DNS service with current public IP
 update_url = parser.get('general', 'update_url')
 updater = utils.updater.Updater(update_url)
-updater.update_dns()
+try:
+	updater.update_dns()
+except:
+	logger.log_error('Problem updating your Dynamic DNS.')
+	sys.exit()
 
 # Store current public IP in the cache file
-cacher.store_ip(current_ip)
+try:
+	cacher.store_ip(current_ip)
+except:
+	logger.log_error('Problem writing to IP cache file.')
+	sys.exit()
+	
 # Log that the address was updated if enabled in config
 if parser.getboolean('logging', 'log_changes'):
 	logger.log_change(old_ip, current_ip)
