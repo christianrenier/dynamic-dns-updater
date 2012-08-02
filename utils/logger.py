bb52768434015f2de85f96936c9993ba5a1b3ff0
Christian Renier <chris@christianrenier.com>
2012-08-02 14:06:36 -0400

    Consolidated code in the Logger class.

--- utils/logger.py
+++ utils/logger.py
@@ -5,19 +5,23 @@ class Logger:
 	def __init__(self, log_location):
 		self.log_location = log_location
 
+	def get_datetime(self):
+		return datetime.datetime.now().ctime()
+
+	def append_message(self, log_message):
+		file = open(self.log_location, 'a')
+		file.write(log_message)
+		file.close()
+
 	def log_error(self, error_message):
 		return
 
 	def log_no_change(self, ip):
-		now = datetime.datetime.now().ctime()
+		now = self.get_datetime()
 		message = "%s: %s remains unchanged.\n" % (now, ip)
-		file = open(self.log_location, 'a')
-		file.write(message)
-		file.close()
+		self.append_message(message)
 
 	def log_change(self, previous_ip, new_ip):
-		now = datetime.datetime.now().ctime()
+		now = self.get_datetime()
 		message = "%s: %s has been updated to %s.\n" % (now, previous_ip, new_ip)
-		file = open(self.log_location, 'a')
-		file.write(message)
-		file.close()
+		self.append_message(message)
