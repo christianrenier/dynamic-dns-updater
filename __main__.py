a2d01685e38d89dc64a03ae22132e5ca145d442f
Christian Renier <chris@christianrenier.com>
2012-08-09 17:49:26 -0400

    Rewrote main.
    Main now includes mailing, enhanced error checking, more organized
    code, more modular code, and more readable code.

--- __main__.py
+++ __main__.py
@@ -3,64 +3,90 @@ import os
 import sys
 import utils
 
-# Load configuration options from config file
-parser = ConfigParser.SafeConfigParser()
-parser.read(os.path.dirname(__file__) + '/config.cfg')
-write_errors = parser.getboolean('logging', 'log_errors')
 
-# Create error message dictionary
+
+## Create global names and functions ##
+
+# Load file locations and configuration options
+site_list_location = os.path.dirname(__file__) + '/sitelist.txt'
+parser = ConfigParser.RawConfigParser()
+parser.read(os.path.dirname(__file__) + '/config.cfg')
+general = dict(parser.items('general'))
+gmail_account = dict(parser.items('gmail_account'))
+write_error = parser.getboolean('logging', 'log_errors')
+write_change = parser.getboolean('logging', 'log_changes')
+write_unchanged = parser.getboolean('logging', 'log_unchanged')
+receiver = parser.get('mailing', 'receiver_address')
+mail_error = parser.getboolean('mailing', 'send_errors')
+mail_change = parser.getboolean('mailing', 'send_changes')
+
+# Tracks if creating a logger failed
+logger = False
+# Tracks if creating a mailer failed
+mailer = False
+
+# Dictionary of error codes and their corresponding messages
 error_messages = {
+			'invalid_login' : 'Your Gmail username or password is incorrect.',
+			'logger_missing' : 'Problem writing to log file.',
 			'read_cache' : 'Problem reading from IP cache.',
+			'read_sitelist' : 'Problem reading the sitelist.',
+			'empty_url' : 'You have not provided an update URL.',
 			'check_ip' : 'Problem checking your IP address.',
-			'update_dns' : 'Problem updating your Dynamic DNS.',
-			'write_cache' : 'Problem writing to IP cache.'
+			'update_dns' : 'Problem updating your Dynamic DNS.'
 			}
 
-# Create a Logger object to log chosen events
-log_file_location = parser.get('general', 'log_file')
-logger = utils.logger.Logger(log_file_location)
-
-# Retrieve IP address stored from previous run
-ip_cache_location = parser.get('general', 'ip_cache_file')
-try:
-	cacher = utils.cacher.Cacher(ip_cache_location)
-except:
-	if write_errors: logger.log_error('read_cache')
+# Handles logging and mailing of errors as enabled by the user
+def error_processor(code):
+	if write_error and logger: logger.log_error(error_messages[code])
+	if mail_error and mailer: mailer.send_error(receiver, error_messages[code])
 	sys.exit()
+
+
+
+## Create instances of utility classes ##
+
+# Only create logger object if user has chosen to log any events
+if write_error or write_change or write_unchanged:
+	try: logger = utils.logger.Logger(general['log_file'])
+	except: logger = False
+# Only create mailer object if user has chosen to mail any events
+if mail_error or mail_change:
+	try: mailer = utils.mailer.Mailer(
+		gmail_account['gmail_user'],
+		gmail_account['gmail_password'])
+	except: error_processor('invalid_login')
+# Notify user by mail that initializing a logger has failed if they
+# enabled any logging
+if not logger and mailer:
+	if write_error or write_change or write_unchanged:
+		error_processor('logger_missing')
+
+try: cacher = utils.cacher.Cacher(general['ip_cache_file'])
+except: error_processor('read_cache')
+try: checker = utils.checker.Checker(site_list_location)
+except: error_processor('read_sitelist')
+try: updater = utils.updater.Updater(general['update_urls'])
+except: error_processor('empty_url')
+
+
+
+## Main ##
+
 old_ip = cacher.get_ip()
 
-# Retrieve current public facing IP address
-site_list_location = os.path.dirname(__file__) + '/sitelist.txt'
-checker = utils.checker.Checker(site_list_location)
-try:
-	current_ip = checker.get_ip()
-except:
-	if write_errors: logger.log_error('check_ip')
-	sys.exit()
+try: current_ip = checker.get_ip()
+except: error_processor('check_ip')
 
-# Exit this script if IP address has not changed
+# If IP has not changed, exit the program
 if old_ip == current_ip:
-	# Log that the address was not updated if enabled in config
-	if parser.getboolean('logging', 'log_no_changes'):
+	if write_unchanged:
 		logger.log_no_change(old_ip)
-	sys.exit()
+		sys.exit()
 
-# Update Dynamic DNS service with current public IP
-update_urls = parser.get('general', 'update_urls')
-updater = utils.updater.Updater(update_urls)
-try:
-	updater.update_dns()
-except:
-	if write_errors: logger.log_error('update_dns')
-	sys.exit()
+try: updater.update_dns()
+except: error_processor('update_dns')
 
-# Store current public IP in the cache file
-try:
-	cacher.store_ip(current_ip)
-except:
-	if write_errors: logger.log_error('write_cache')
-	sys.exit()
-	
-# Log that the address was updated if enabled in config
-if parser.getboolean('logging', 'log_changes'):
-	logger.log_change(old_ip, current_ip)
+cacher.store_ip(current_ip)
+if write_change: logger.log_change(old_ip, current_ip)
+if mail_change and mailer: mailer.send_change(receiver, old_ip, current_ip)
