acf1afbdc7f8487d7aef0c0c8570e9058f44dca6
Christian Renier <chris@christianrenier.com>
2012-08-08 20:06:53 -0400

    Adds protocol to URL in case user doesn't.

--- utils/updater.py
+++ utils/updater.py
@@ -5,7 +5,7 @@ class Updater:
 	"""Visits the update URL for a Dynamic DNS service."""
 
 	def __init__(self, update_urls):
-
+		
 		# Make a list out of the string split by comma
 		self.update_urls = update_urls.split(',')
 
@@ -13,6 +13,10 @@ class Updater:
 
 		"""Connects to each update URL, then disconnects."""
 		for url in self.update_urls:
+
+			# Adds protocol if address does not contain it
+			if 'http://' not in url: url = 'http://' + url
+
 			request = urllib.urlopen(url)
 			request.close()
 		
